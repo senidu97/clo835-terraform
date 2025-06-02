ff2c3be7510f34e2f8a30e5f149e15d819ce7be2
senindumendis <senidumendis@gmail.com>
2025-06-02 17:33:41 -0400

    adding the initial commit after local testing

--- ec2/backend.tf
+++ ec2/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "clo835-assignment1-senindumendis"
+    bucket         = "clo835-assignment1-senindumendis" #replace with your s3 bucket name
     key            = "ec2/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
